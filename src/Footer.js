a8849aad67e2e4dc9a93de5b2bbce87a2bb034dd
Tochi Oramasionwu <tochi.orams@gmail.com>
2023-03-02 20:37:36 -0500

    Added semantic components and meta tags

---
+++ src/Footer.js
@@ -0,0 +1,63 @@
+return (
+    <footer>
+        <div className="FooterLogo">
+            <a href="#">
+                <img src="src/Assets/Logo 2.png" alt="Home" />
+            </a>
+        </div>
+        <div className="SiteNav">
+            <ul>
+                <li className="ListTitle">
+                    Navigation
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">About</a>
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Menu</a>
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Reservations</a>
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Order Online</a>
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Login</a>
+                </li>
+            </ul>
+        </div>
+        <div className="Contact">
+            <ul>
+                <li className="ListTitle">
+                    Contact
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Email</a>
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Phone Number</a>
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Address</a>
+                </li>
+            </ul>
+        </div>
+        <div className="SocialMedia">
+            <ul>
+                <li className="ListTitle">
+                    Socials
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Instagram</a>
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Facebook</a>
+                </li>
+                <li className="FootItem">
+                    <a className="FootLink" href="#">Tik Tok</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+)
